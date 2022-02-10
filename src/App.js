f053f1352301123ba321fd0e9209a0155ee44215
emanet <mehmetcanemanet@gmail.com>
2022-02-10 20:31:40 +0300

    feat: router add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Locations from "./pages/Locations";
+import Residents from "./pages/Residents";
+import "./App.css";
 function App() {
   return (
-    <div className="App">
-      <Locations />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/locations" element={<Locations />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/pages/Locations.jsx
+++ src/pages/Locations.jsx
@@ -11,26 +11,39 @@ function Locations() {
   }, [locations]);
   return (
     <div>
-      {locations.results.map((item) => {
-        return (
-          <div
-            style={{
-              display: "inline-block",
-              background: "gray",
-              margin: "1em",
-            }}
-            key={item.id}
-          >
-            <h3>Location Info</h3>
-            <div>
-              {"Name: " + item.name}
-              <br />
-              {"Type: " + item.type}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "center",
+        }}
+      >
+        {locations.results.map((item) => {
+          return (
+            <div
+              style={{
+                display: "flex",
+                background: "#E9DAC1",
+                width: "10em",
+                height: "9em",
+                textAlign: "center",
+                flexDirection: "column",
+                margin: "1em",
+              }}
+              key={item.id}
+            >
+              <h3>Location Info</h3>
+              <div>
+                {"Name: " + item.name}
+                <br />
+                {"Type: " + item.type}
+              </div>
             </div>
-          </div>
-        );
-      })}
-      <div>
+          );
+        })}
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
         {[...Array(pageCount).keys()].map((i) => {
           return (
             <button onClick={() => setCurrentPage(i + 1)} id={i} key={i}>
