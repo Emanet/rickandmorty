0e1abdcc33ed918d3222e272b35ff22538ba4354
emanet <mehmetcanemanet@gmail.com>
2022-02-11 10:35:58 +0300

    fix: get back locations button

--- src/pages/Residents.jsx
+++ src/pages/Residents.jsx
@@ -1,22 +1,30 @@
 import { useParams } from "react-router-dom";
 import useResidents from "../hooks/useResidents";
 import Card from "../components/Card/Card";
+
 function Residents() {
   const { location_id } = useParams();
   console.log(location_id);
   const residents = useResidents(location_id);
   return (
-    <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr" }}>
-      {residents.map((item) => (
-        <Card
-          name={item.name}
-          image={item.image}
-          species={item.species}
-          type={item.type}
-          gender={item.gender}
-          origin={item.origin.name}
-        />
-      ))}
+    <div>
+      <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr" }}>
+        {residents.map((item) => (
+          <Card
+            name={item.name}
+            image={item.image}
+            species={item.species}
+            type={item.type}
+            gender={item.gender}
+            origin={item.origin.name}
+          />
+        ))}
+      </div>
+      <a style={{ textDecoration: "none" }} href="/">
+        <button style={{ display: "flex", margin: "0 auto" }}>
+          Get back to locations.
+        </button>
+      </a>
     </div>
   );
 }
